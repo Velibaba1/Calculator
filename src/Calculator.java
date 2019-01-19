7233bcf0e0f73960c503cc200f5d9a98f86ca81e
Velibaba1 <46760840+Velibaba1@users.noreply.github.com>
2019-01-19 13:10:25 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,6 +8,6 @@ public class Calculator {
         double result =num1-num2;
         return result;
     }
-    public static double multiply(double num1,num2){
+    public static double multiply(double num1,double num2){
         return num1*num2;
 }
